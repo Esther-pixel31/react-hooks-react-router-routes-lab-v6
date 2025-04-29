9a37b6ec6114f5810caed3a1fcc08973f0c3ec02
EstherMutua <esthermutua16@gmail.com>
2025-04-29 23:29:38 +0300

    lab done

--- src/pages/Directors.jsx
+++ src/pages/Directors.jsx
@@ -1,12 +1,31 @@
+// import { useEffect, useState } from "react";
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
 
 function Directors() {
+  const [directors, setDirectors] = useState([])
+  useEffect(() => {
+    fetch('http://localhost:4000/directors')
+    .then(res => res.json())
+    .then(arr => setDirectors(arr))
+  }, [])
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
+        <h1>Directors Page</h1>
+        {directors.map(director =>{
+          return <article key={director.id}>
+            <h2>{director.name}</h2>
+            <ul>
+              {director.movies.map(movie=> {
+                return <li key={movie}>{movie}</li>
+              })}
+            </ul>
+          </article>
+        })}
         {/* Director info here! */}
       </main>
     </>
--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,16 +1,36 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
+import NavBar from '../components/NavBar';
+import { useParams } from 'react-router-dom';
 
 function Movie() {
-  return (
-    <>
-      <header>
-        {/* What component should go here? */}
-      </header>
-      <main>
-        {/* Movie info here! */}
-      </main>
-    </>
-  );
-};
+
+  const params = useParams()
+  const id = params.id
+  const [movie, setMovie] = useState({})
+  useEffect(() => {
+    fetch(`http://localhost:4000/movies/${id}`)
+    .then(res => res.json())
+    .then(movie => {
+      setMovie(movie)
+    })
+    .catch(error => console.log(error))
+  }, [id])
+  if (!movie.title) {
+    return <h2>Loading...</h2>
+  }
+	return (
+		<>
+			<header>
+				<NavBar />
+			</header>
+			<main>
+				<h1>{movie.title}</h1>
+        <p>{movie.time} minutes</p>
+        {movie?.genres.map((genre, index) => <span key={index}>{genre}</span> ) }
+			</main>
+		</>
+	);
+}
 
 export default Movie;
